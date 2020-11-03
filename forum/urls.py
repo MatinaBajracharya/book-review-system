b233ed5094b20d4fbcd68de7e90b6df88a9813d9
matina13 <58813551+matina13@users.noreply.github.com>
2020-11-03 23:55:56 +0545

    Post CRUD

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', views.home, name='forum-home'),
     path('about/', views.about, name='forum-about'),
-    path('forum/', views.forum, name='forum-page'),
+    path('forum/', PostListView.as_view(), name='forum-page'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Matina Bajracharya',
-        'title': 'Blog Post 1',
-        'content': 'First post',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Matina Bajracharya',
-        'title': 'Blog Post 1',
-        'content': 'First post',
-        'date_posted': 'August 27, 2018'
-    }
-]
-
-
-
 
 def home(request):
     return render(request, 'forum/home.html', {'title': 'Home'})
@@ -33,3 +17,46 @@ def forum(request):
         'title': 'Forum'
     }
     return render(request, 'forum/forum.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'forum/forum.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/forum'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    
