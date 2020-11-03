b1ea10b9e095429ee46795c766dfcffb47c7d06a
matina13 <58813551+matina13@users.noreply.github.com>
2020-11-03 21:53:45 +0545

    Login and User Profile

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -10,9 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for { username }!')
-            return redirect('forum-home')
+            messages.success(request, f'Welcome to our community { username }! Please login to continue.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
 
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
