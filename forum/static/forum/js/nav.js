c0b24643b80975d477d97b1edaf1e9015b9bac14
matina13 <58813551+matina13@users.noreply.github.com>
2021-03-01 12:02:11 +0545

    Minor UI Changes

--- forum/static/forum/js/nav.js
+++ forum/static/forum/js/nav.js
@@ -13,16 +13,16 @@ function tabs(panelIndex) {
 }
 tabs(0);
 
-const bio = document.querySelector('.bio');
+// const bio = document.querySelector('.bio');
 
-function bioText() {
-    bio.oldText = bio.innerText;
-    bio.innerText = bio.innerText.substring(0, 100) + "...";
-    bio.innerHTML += "&nbsp;" + `<span onclick='addLength()' id='see-more-bio'>See More</span>`
-}
-bioText();
+// function bioText() {
+//     bio.oldText = bio.innerText;
+//     bio.innerText = bio.innerText.substring(0, 100) + "...";
+//     bio.innerHTML += "&nbsp;" + `<span onclick='addLength()' id='see-more-bio'>See More</span>`
+// }
+// bioText();
 
-function addLength() {
-    bio.innerHTML = bio.oldText;
-    bio.innerHTML += "&nbsp;" + `<span onclick='bioText()' id='see-less-bio'>See Less</span>`
-}
+// function addLength() {
+//     bio.innerHTML = bio.oldText;
+//     bio.innerHTML += "&nbsp;" + `<span onclick='bioText()' id='see-less-bio'>See Less</span>`
+// }
